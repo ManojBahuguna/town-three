3a0f0b614b038df9a9d430a1201b1cf46d9c58ac
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2023-01-27 13:29:47 +0530

    feat: add pan distance limit

---
+++ src/Control.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from "react";
+import { OrbitControls, OrbitControlsProps } from "@react-three/drei";
+
+const maxPanDistance = 50;
+export function Control() {
+  const controlsRef = useRef<any>();
+
+  useEffect(() => {
+    const onControlsChange = () => {
+      if (controlsRef.current.target.x < -maxPanDistance) {
+        controlsRef.current.target.x = -maxPanDistance;
+      } else if (controlsRef.current.target.x > maxPanDistance) {
+        controlsRef.current.target.x = maxPanDistance;
+      }
+
+      if (controlsRef.current.target.z < -maxPanDistance) {
+        controlsRef.current.target.z = -maxPanDistance;
+      } else if (controlsRef.current.target.z > maxPanDistance) {
+        controlsRef.current.target.z = maxPanDistance;
+      }
+
+      if (controlsRef.current.target.y < 1) {
+        controlsRef.current.target.y = 1;
+      } else if (controlsRef.current.target.y > maxPanDistance) {
+        controlsRef.current.target.y = maxPanDistance;
+      }
+    };
+
+    // limit pan distance
+    controlsRef.current.addEventListener("change", onControlsChange);
+
+    return () => {
+      controlsRef.current.removeEventListener("change", onControlsChange);
+    };
+  }, []);
+
+  return (
+    <OrbitControls
+      ref={controlsRef}
+      dampingFactor={0.02}
+      minDistance={20}
+      maxDistance={110}
+      maxPolarAngle={Math.PI * 0.49}
+    />
+  );
+}
--- src/Town.tsx
+++ src/Town.tsx
@@ -1,19 +1,20 @@
-import { Canvas } from "@react-three/fiber";
-import { Building, BuildingData } from "./Building";
-import { OrbitControls } from "@react-three/drei";
-import { Stars } from "@react-three/drei/core";
+import { useEffect, useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls, Stars, useCamera } from "@react-three/drei";
+import { DoubleSide, Vector3 } from "three";
 import { BuildingRow } from "./BuildingsRow";
-import { DoubleSide } from "three";
+import { type BuildingData } from "./Building";
+import { Control } from "./Control";
 
 const leftBuildings: BuildingData[] = [
-  { floors: 5, width: 10, depth: 10 },
+  { floors: 5, width: 10, depth: 13 },
   { floors: 3, width: 20, depth: 10 },
   { floors: 8, width: 10, depth: 20 },
 ];
 
 export function Town() {
   return (
-    <Canvas shadows camera={{ position: [10, 30, 50] }}>
+    <Canvas shadows camera={{ position: [10, 40, 60], far: 1000 }}>
       {/* Objects */}
       <mesh position-x={-35} position-z={15}>
         <BuildingRow rotation-y={Math.PI / 2} rowData={leftBuildings} />
@@ -23,22 +24,17 @@ export function Town() {
       <color attach="background" args={[0x222232]} />
       <hemisphereLight args={[0x443333, 0xeeeeff, 1]} />
       <ambientLight intensity={0.3} />
-      <Stars radius={200} />
+      <Stars radius={300} fade />
       <fog attach="fog" args={[0x664444, -60, 130]} />
 
       {/* Plane */}
       <mesh rotation-x={-Math.PI / 2} receiveShadow>
-        <planeGeometry args={[80, 80]} />
-        <meshLambertMaterial color={0x252233} side={DoubleSide} />
+        <circleGeometry args={[500]} />
+        <meshLambertMaterial color={0x252233} />
       </mesh>
 
       {/* Controls */}
-      <OrbitControls
-        dampingFactor={0.02}
-        maxDistance={130}
-        maxPolarAngle={Math.PI * 0.495}
-        enablePan={false}
-      />
+      <Control />
     </Canvas>
   );
 }
